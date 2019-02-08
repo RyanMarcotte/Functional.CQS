46e7ddb991d236d0bb6c06f5e6b116a9b9c5784b
Ryan Marcotte <RyanM@iqmetrix.com>
2019-02-08 15:17:59 -0600

    cleanup

--- src/Functional.CQS.AOP.CommonTestInfrastructure/DummyObjects/DummyAsyncCommandHandlerThatFails.cs
+++ src/Functional.CQS.AOP.CommonTestInfrastructure/DummyObjects/DummyAsyncCommandHandlerThatFails.cs
@@ -8,9 +8,6 @@ namespace Functional.CQS.AOP.CommonTestInfrastructure.DummyObjects
 	/// </summary>
 	public class DummyAsyncCommandHandlerThatFails : IAsyncCommandHandler<DummyAsyncCommandThatFails, DummyAsyncCommandError>
 	{
-		private static readonly DummyAsyncCommandThatFails _command = new DummyAsyncCommandThatFails();
-		private static readonly Result<Unit, DummyAsyncCommandError> _result = Result.Failure<Unit, DummyAsyncCommandError>(new DummyAsyncCommandError());
-
 		/// <summary>
 		/// Handles the command asynchronously.
 		/// </summary>
@@ -19,7 +16,7 @@ namespace Functional.CQS.AOP.CommonTestInfrastructure.DummyObjects
 		/// <returns></returns>
 		public async Task<Result<Unit, DummyAsyncCommandError>> HandleAsync(DummyAsyncCommandThatFails command, CancellationToken cancellationToken)
 		{
-			return await Task.Run(() => _result, cancellationToken);
+			return await Task.Run(() => Result.Failure<Unit, DummyAsyncCommandError>(new DummyAsyncCommandError()), cancellationToken);
 		}
 	}
 }
