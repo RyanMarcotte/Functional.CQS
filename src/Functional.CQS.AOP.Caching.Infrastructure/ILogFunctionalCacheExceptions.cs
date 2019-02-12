c1aced3ee1451ed615ec5e31739288b29dcd10f6
Ryan Marcotte <RyanM@iqmetrix.com>
2019-02-12 10:59:28 -0600

    contracts

---
+++ src/Functional.CQS.AOP.Caching.Infrastructure/ILogFunctionalCacheExceptions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Functional.CQS.AOP.Caching.Infrastructure
+{
+	/// <summary>
+	/// Interface for logging any exceptions that occur when interacting with the cache.
+	/// </summary>
+	public interface ILogFunctionalCacheExceptions
+	{
+		/// <summary>
+		/// Log an exception that occurred while executing a specific query.
+		/// </summary>
+		/// <param name="queryType">The query type.</param>
+		/// <param name="resultType">The result type.</param>
+		/// <param name="cacheKey">The cache key.</param>
+		/// <param name="exception">The exception.</param>
+		void LogException(Type queryType, Type resultType, string cacheKey, Exception exception);
+
+		/// <summary>
+		/// Log an exception that occurred while overwriting or invalidating the cache.
+		/// </summary>
+		/// <param name="exception">The exception.</param>
+		void LogException(Exception exception);
+	}
+}
---
+++ src/Functional.CQS.AOP.Caching.Infrastructure/ILogFunctionalCacheHitsAndMisses.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Functional.CQS.AOP.Caching.Infrastructure
+{
+	/// <summary>
+	/// Interface for logging cache hits and misses.
+	/// </summary>
+	public interface ILogFunctionalCacheHitsAndMisses
+	{
+		/// <summary>
+		/// Log a cache hit.
+		/// </summary>
+		/// <param name="queryType">The query type.</param>
+		/// <param name="resultType">The result type.</param>
+		/// <param name="cacheKey">The cache key.</param>
+		void LogCacheHit(Type queryType, Type resultType, string cacheKey);
+
+		/// <summary>
+		/// Log a cache miss.
+		/// </summary>
+		/// <param name="queryType">The query type.</param>
+		/// <param name="resultType">The result type.</param>
+		/// <param name="cacheKey"></param>
+		void LogCacheMiss(Type queryType, Type resultType, string cacheKey);
+	}
+
+	// ReSharper disable once InconsistentNaming
+	/// <summary>
+	/// Extension methods for <see cref="ILogFunctionalCacheHitsAndMisses"/>.
+	/// </summary>
+	public static class ExtensionsFor_ILogFunctionalCacheHitsAndMisses
+	{
+		/// <summary>
+		/// Log a cache hit or miss.
+		/// </summary>
+		/// <param name="logger">The logger.</param>
+		/// <param name="isCacheHit">Indicates if we are logging a cache hit or miss.</param>
+		/// <param name="queryType">The query type.</param>
+		/// <param name="resultType">The result type.</param>
+		/// <param name="cacheKey">The cache key.</param>
+		public static void DoCacheLogging(this ILogFunctionalCacheHitsAndMisses logger, bool isCacheHit, Type queryType, Type resultType, string cacheKey)
+		{
+			if (logger == null) throw new ArgumentNullException(nameof(logger));
+			if (queryType == null) throw new ArgumentNullException(nameof(queryType));
+			if (resultType == null) throw new ArgumentNullException(nameof(resultType));
+			if (cacheKey == null) throw new ArgumentNullException(nameof(cacheKey));
+
+			if (isCacheHit)
+				logger.LogCacheHit(queryType, resultType, cacheKey);
+			else
+				logger.LogCacheMiss(queryType, resultType, cacheKey);
+		}
+	}
+}
