ab2253fd908dbbd1f12f4aac9e5408a33962d965
Ryan Marcotte <RyanM@iqmetrix.com>
2019-02-14 07:44:28 -0600

    added caching installation project

---
+++ src/Functional.CQS.AOP.IoC.SimpleInjector.Caching/TypeExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using Functional;
+using Functional.CQS.AOP.Caching;
+using Functional.CQS.AOP.IoC.SimpleInjector;
+using Functional.CQS.AOP.IoC.SimpleInjector.Models;
+
+// ReSharper disable once CheckNamespace
+namespace SimpleInjector
+{
+	internal static class TypeExtensions
+	{
+		public static Option<QueryAndResultType> GetGenericParametersForQueryCachingStrategyType(this Type type)
+		{
+			return type.GetClosedGenericInterfaceTypeFromOpenGenericInterfaceTypes(_queryCachingStrategyTypeCollection).Map(queryHandlerInterface =>
+			{
+				var queryType = queryHandlerInterface.GenericTypeArguments[0];
+				var resultType = queryHandlerInterface.GenericTypeArguments[1];
+				return new QueryAndResultType(queryType, resultType);
+			});
+		}
+
+		private static readonly IEnumerable<Type> _queryCachingStrategyTypeCollection = new HashSet<Type>()
+		{
+			typeof(IQueryResultCachingStrategy<,>)
+		};
+	}
+}
